7b0759635d4438e0f983935076f7453d441dbae9
Jørgen G. Nielsen <jgn@dbc.dk>
2020-12-11 09:50:11 +0100

    update code from /DBCDK/bibdk/tree/master/modules/bibdk_subject_hierarchy

--- theme/bibdk-subject-hierarchy-item.tpl.php
+++ theme/bibdk-subject-hierarchy-item.tpl.php
@@ -5,24 +5,26 @@
  */
 ?>
 
-<div class="themes__sublists">
-  <div class="themes__close-button icon icon-blue-x">&nbsp;</div>
-  <div class="themes__breadcrumb">
+<div class="subjects-sublists">
+
+  <div class="subjects-close-button"><?php echo drupal_render($close_button); ?></div>
+
+  <div class="subjects-breadcrumb">
     <?php echo drupal_render($breadcrumbs); ?>
   </div>
-  <div class="themes__sublist">
+
+  <div class="subjects-sublists-heading">
+    <?php echo drupal_render($heading); ?>
+  </div>
+
+  <div class="subjects-sublist">
     <ul>
-      <?php foreach ($variables['hierarchy']['term'] as $key => $item) {
-        if (!empty($item['cql']) && empty($item['term'])) {
-          $url = 'search/' . $variables['search_path'] . '/' . trim($item['cql']);
-          $attributes['attributes'] = array();
+      <?php
+        foreach ($variables['hierarchy'] as $key => $item) {
+            echo render($item);
         }
-        else {
-          $url = 'bibdk_subject_hierarchy/nojs/' . $variables['current_key'] . ',' . $key;
-          $attributes['attributes']['class'] = array('use-ajax', 'nesting');
-        }
-        echo '<li>' . l($item['ord'], $url, $attributes) . "</li>\n";
-      } ?>
+      ?>
     </ul>
   </div>
+
 </div>
--- theme/bibdk-subject-hierarchy.tpl.php
+++ theme/bibdk-subject-hierarchy.tpl.php
@@ -6,25 +6,53 @@
  */
 ?>
 
-<div id="bibdk-subject-hierarchy" class="themes clearfix">
-  <div class="container clearfix">
-    <div class="clearfix">
-      <div class="bibdk-subject-hierarchy-header">
-        <?php print $header; ?>
+<div id="bibdk-subject-hierarchy">
+
+  <div class="bibdk-subject-hierarchy-header">
+
+    <div class="bibdk-subject-hierarchy-header-title">
+      <?php print $header; ?>
+    </div>
+
+    <div class="bibdk-subject-hierarchy-header-helptext">
+      <p class="helptext popover-button" title="<?php print $helptext; ?>">
+        <a href="#" tabindex="0" aria-label="<?php print t('Help', array(), array('context' => 'bibdk_theme')); ?>" role="button">?</a>
+      </p>
+      <div class="popover element-wrapper linkme-wrapper visuallyhidden hide-text">
+        <p class="user-msg ">
+          <?php print $helptext; ?>
+        </p>
+        <a href="#"  tabindex="0" aria-label="<?php print t('Close help', array(), array('context' => 'bibdk_theme')); ?>"
+           role="button" class="close icon icon-left icon-red-x ">
+        </a>
       </div>
-      <div class="bibdk-subject-hierarchy-helptext">
-        <?php print $helptext; ?>
+    </div>
+
+    <div class="subject-hierarchy-searchfield">
+      <div class="show-for-large-up">
+        <?php print drupal_render($searchfield_link); ?>
       </div>
-      <div class="subject-hierarchy-searchfield">
+      <div class="hide-for-large-up">
+        <?php
+          $searchfield_link['#printed'] = FALSE;
+          $searchfield_link['#attributes']['data-options'] = 'align:bottom';
+          print drupal_render($searchfield_link);
+        ?>
+      </div>
+
+      <div id="searchfield-dropdown" data-dropdown-content class="f-dropdown content medium" aria-hidden="true" tabindex="-1">
         <?php print drupal_render($searchfield); ?>
       </div>
     </div>
-    <div id="bibdk-subject-hierarchy-content">
-    <?php foreach ($rows as $row) : ?>
-      <?php print $row; ?>
-    <?php endforeach; ?>
-    </div>
-    <div id="bibdk-subject-hierarchy-searchresult">
-    </div>
-    </div>
+
+  </div>
+
+  <div id="bibdk-subject-hierarchy-content" class="bibdk-subject-hierarchy-inner clearfix">
+  <?php foreach ($rows as $row) : ?>
+    <?php print $row; ?>
+  <?php endforeach; ?>
+  </div>
+
+  <div id="bibdk-subject-hierarchy-searchresult"></div>
+
 </div>
